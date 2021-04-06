0f9eeb74200ab0c7f0c84d02b1c99a0f2015f6ae
Haris Zubair <haris@Haris-MacBook-Air.local>
2021-04-06 22:50:08 +0500

    fix for local merges on cpanel

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,9 +1,9 @@
 
                                 <!-- footer @s -->
-                                <div class="nk-footer" style="bottom: 0;position: fixed;">
+                                <div class="nk-footer">
                                     <div class="container wide-xl">
                                         <div class="nk-footer-wrap g-2">
-                                            <div class="nk-footer-copyright"> &copy; 2020 DashLite. Template by <a href="#">Softnio</a>
+                                            <div class="nk-footer-copyright"> &copy; 2021 AZ Insurance. All Rights Reserved.
                                             </div>
                                             <div class="nk-footer-links">
                                                 <ul class="nav nav-sm">
@@ -30,8 +30,6 @@
 
 
     <script src="{{asset('js/jquery.min.js')}}"></script>
-    
-    
  <script>
    $('.dark-switch').on('click', function (e) {
     $.ajax(
@@ -46,6 +44,7 @@
     <script src="{{asset('js/scripts.js')}}"></script>
 
     <script src="{{asset('js/sweetalert.min.js')}}"></script>
+    <script src="{{asset('js/gd-default.js')}}"></script>
     <!-- Include this after the sweet alert js file -->
     @include('sweet::alert')
      @yield('footer')
